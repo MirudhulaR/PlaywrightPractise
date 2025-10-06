097af5388865717f5946c6c41a122033952370d5
Your Name <you@example.com>
2025-10-06 15:21:36 +1100

    Added test for contact us page - module 4

---
+++ jupiter-toys/e2e/contact.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('Contact form submission', async ({ page }) => {
+  // Navigate to the contact page
+  await page.goto('https://jupiter.cloud.planittesting.com/#/contact');
+
+    // Fill in the contact form
+  await page.fill('input[name="forename"]', 'John');
+  await page.fill('input[name="surname"]', 'Doe');
+  await page.fill('input[name="email"]', 'john.doe@example.com');
+  await page.fill('input[name="telephone"]', '1234567890');
+  await page.fill('textarea[name="message"]', 'This is a test message.');
+    
+    // Submit the form
+    // npx playwright codegen
+  await page.getByRole('link', { name: 'Submit' }).click();
+
+  // Wait for the success message
+  const successMessage = await page.waitForSelector('.alert-success');
+  expect(successMessage).toBeTruthy();
+
+   // Verify the success message content
+  const successMessageText = await successMessage.textContent();
+  expect(successMessageText).toContain('Thanks John, we appreciate your feedback.');
+
+});
